6915f774809836160f261e224b4e303eca56df7e
roman89ua <roman89ua@gmail.com>
2021-08-19 12:43:11 +0300

    handle scss, css, images and svg formats in webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './styles.scss';
+import REACT_LOGO from './React-logo.png';
+import REACT_LOGO_SVG from './react-logo.svg';
 
 export const App = () => {
-    return <h1>React typescript webpack</h1>
+    return (
+        <div className="centered">
+            <h1>React typescript webpack</h1>
+            <img src={REACT_LOGO} alt="React logo" width="50%" height="50%"/>.
+            <img src={REACT_LOGO_SVG} alt="React logo svg" width="50%" height="50%"/>.
+        </div>
+    )
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -17,6 +17,25 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: "asset/resource"
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/i,
+                type: 'asset/inline'
+            },
         ],
     },
     output: {
