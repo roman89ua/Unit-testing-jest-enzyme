4fa3f92b7314bc12eb207007b9284a4a027784f7
roman89ua <roman89ua@gmail.com>
2021-08-20 18:09:33 +0300

    react-refresh-webpack-plugin and script luncher changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './styles.scss';
 import REACT_LOGO from './React-logo.png';
 import REACT_LOGO_SVG from './react-logo.svg';
+import {ClickCounter} from "./ClickCounter";
 
 export const App = () => {
     return (
         <div className="centered">
-            <h1>React typescript webpack - {process.env.NODE_ENV} {process.env.name}</h1>
+            <h1>EDITED react & typescript & webpack - {process.env.NODE_ENV} {process.env.name}</h1>
             <img src={REACT_LOGO} alt="React logo" width="50%" height="50%"/>.
             <img src={REACT_LOGO_SVG} alt="React logo svg" width="50%" height="50%"/>.
+            <ClickCounter />
         </div>
     )
 }
---
+++ src/ClickCounter.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+export const ClickCounter = () => {
+    const [count, setCount] = useState(0);
+    const countClickHandler = () => {
+        setCount(count + 1);
+    }
+    return (
+        <div>
+           <button onClick={countClickHandler}>Count: {count}</button>
+        </div>
+    )
+}
