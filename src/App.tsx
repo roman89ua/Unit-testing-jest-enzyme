0c2e5a29df29e63565ed540da255a59fd964ef68
roman89ua <roman89ua@gmail.com>
2021-08-22 19:30:49 +0300

    prettier set up for ide and for the project

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import './styles.scss';
 import REACT_LOGO from './React-logo.png';
 import REACT_LOGO_SVG from './react-logo.svg';
-import {ClickCounter} from "./ClickCounter";
+import { ClickCounter } from './ClickCounter';
 
 export const App = () => {
-    return (
-        <div className="centered">
-            <h1>EDITED react & typescript & webpack - {process.env.NODE_ENV} {process.env.name}</h1>
-            <img src={REACT_LOGO} alt="React logo" width="50%" height="50%"/>.
-            <img src={REACT_LOGO_SVG} alt="React logo svg" width="50%" height="50%"/>.
-            <ClickCounter />
-        </div>
-    )
-}
+  return (
+    <div className="centered">
+      <h1>
+        EDITED react & typescript & webpack - {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      <img src={REACT_LOGO} alt="React logo" width="50%" height="50%" />.
+      <img src={REACT_LOGO_SVG} alt="React logo svg" width="50%" height="50%" />.
+      <ClickCounter />
+    </div>
+  );
+};
--- src/ClickCounter.tsx
+++ src/ClickCounter.tsx
@@ -4,10 +4,10 @@ export const ClickCounter = () => {
   const [count, setCount] = useState(0);
   const countClickHandler = () => {
     setCount(count + 1);
-  }
+  };
   return (
     <div>
       <button onClick={countClickHandler}>Count: {count}</button>
     </div>
-  )
-}
+  );
+};
