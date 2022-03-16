8765fbbe8aaad75162e4b440459c62279f8b565e
Roman Kostiuk <roman89ua@gmail.com>
2022-03-16 17:49:15 +0200

    set up updates

--- config/setupTest.js
+++ config/setupTest.js
@@ -1,14 +1,9 @@
-import Enzyme /*, { shallow, render, mount } */ from 'enzyme';
+import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-// import toJson from 'enzyme-to-json';
+import 'jsdom-global/register';
 
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
-
-// global.shallow = shallow;
-// global.render = render;
-// global.mount = mount;
-// global.toJson = toJson;
 
 // Fail tests on any warning
 console.error = (message) => {
