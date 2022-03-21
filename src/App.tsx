b6cfde2add8435fd05eb3226af8ad36197a34236
Roman Kostiuk <roman89ua@gmail.com>
2022-03-21 14:28:16 +0200

    Used instead h1, Title component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React, { ReactNode } from 'react';
-import './styles.scss';
-import REACT_LOGO from './React-logo.png';
-import REACT_LOGO_SVG from './react-logo.svg';
+import REACT_LOGO from './images/React-logo.png';
+import REACT_LOGO_SVG from './images/react-logo.svg';
 import { ClickCounter } from 'components/ClickCounter';
 import { PostList } from 'components/PostList';
+import { Title } from 'components/Title';
+import './styles.scss';
 
 export const App = () => {
   const a = '100';
   const isDevEnv = process.env.NODE_ENV === 'development';
   return (
     <div className="centered">
-      <h1>
-        EDITED react & typescript & webpack - {process.env.NODE_ENV} {process.env.name} {a}
-      </h1>
+      <Title title={`EDITED react & typescript & webpack - ${process.env.NODE_ENV} ${process.env.name} ${a}`} />
       <img
         src={isDevEnv ? REACT_LOGO : REACT_LOGO_SVG}
         alt={isDevEnv ? 'React logo' : 'React logo svg'}
